aa62c532031b0b83140da78e12fd6c6bab15b52e
gleisonnanet <gleisonnanet@gmail.com>
2018-04-06 10:30:21 -0300

    Update menu
    Update menu

--- tema/SouLean/modulos/sidebar/menu.php
+++ tema/SouLean/modulos/sidebar/menu.php
@@ -1,12 +1,25 @@
 <!-- START X-NAVIGATION -->
 <ul class="x-navigation">
     <li class="xn-logo">
-        <a href="index.html">Joli Admin</a>
+        <a href="index.html">Sou Lean</a>
         <a href="#" class="x-navigation-control"></a>
     </li>
-    <li class="xn-title">Navigation</li>
+    <li class="xn-title">Menu lean</li>
     <li>
-        <a href="#"><span class="fa fa-desktop"></span> <span class="xn-text">Dashboard</span></a>
+        <a href="#"><span class="glyphicon glyphicon-dashboard"></span> <span class="xn-text">Dashboard</span></a>
     </li>
+    <li>
+        <a href="#"><span class="fa fa-bullhorn"></span> <span class="xn-text">Fórum</span></a>
+    </li>
+    <li>
+        <a href="#"><span class="fa fa-user"></span> <span class="xn-text">Perfil</span></a>
+    </li>
+    <li>
+        <a href="#"><span class="fa fa-sign-in"></span> <span class="xn-text">Assinatura</span></a>
+    </li>
+    <li>
+        <a href="#"><span class="fa fa-stethoscope"></span> <span class="xn-text">Suporte</span></a>
+    </li>
+
 </ul>
 <!-- END X-NAVIGATION -->

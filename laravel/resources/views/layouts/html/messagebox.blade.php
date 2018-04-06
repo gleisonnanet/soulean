b1075a14b31776006cdecf72edadd928482b629a
gleisonnanet <gleisonnanet@gmail.com>
2018-04-06 18:06:42 -0300

    atualizando header

--- laravel/resources/views/layouts/html/messagebox.blade.php
+++ laravel/resources/views/layouts/html/messagebox.blade.php
@@ -4,15 +4,15 @@
 <div class="message-box animated fadeIn" data-sound="alert" id="mb-signout">
     <div class="mb-container">
         <div class="mb-middle">
-            <div class="mb-title"><span class="fa fa-sign-out"></span> Log <strong>Out</strong> ?</div>
+            <div class="mb-title"><span class="fa fa-sign-out"></span> S<strong>air</strong> ?</div>
             <div class="mb-content">
-                <p>Are you sure you want to log out?</p>
-                <p>Press No if youwant to continue work. Press Yes to logout current user.</p>
+                <p>Tem certeza que deseja sair?</p>
+                <p>Pressione Não se quiser continuar o trabalho. Pressione Sim para sair do usuário atual.</p>
             </div>
             <div class="mb-footer">
                 <div class="pull-right">
-                    <a href="pages-login.html" class="btn btn-success btn-lg">Yes</a>
-                    <button class="btn btn-default btn-lg mb-control-close">No</button>
+                    <a href="pages-login.html" class="btn btn-success btn-lg">Sim</a>
+                    <button class="btn btn-default btn-lg mb-control-close">Não</button>
                 </div>
             </div>
         </div>
